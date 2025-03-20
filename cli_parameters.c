c48aa76ed7149ad5253a84679ef22f7c5976fea8
leedoor <samoshenkoleonid@yandex.ru>
2025-03-20 20:21:43 +0300

    cli_fixes

--- cli_parameters.c
+++ cli_parameters.c
@@ -22,6 +22,8 @@ void cli_parameters_ctor(cli_parameters_t* cli) {
 cli_parameters_t resolve_cli_parameters(int argc, char** argv) {
     cli_parameters_t cli;
     cli_parameters_ctor(&cli);
+    bool overrided_src = false;
+    bool overrided_dest = false;
     for(int i = 1; i < argc; ++i) {
         if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0) {
             printf("This application is compressing ASCII text. It is more useful when compressing some default speech, "
@@ -33,6 +35,9 @@ cli_parameters_t resolve_cli_parameters(int argc, char** argv) {
             exit(0);
         }
         else if (strcmp(argv[i], "--gen-tests") == 0 || strcmp(argv[i], "-g") == 0) {
+            if(cli.action == DECODE) {
+                PARAM_ERROR(-4, "Can`t generate tests if decoding.\n", cli);
+            }
             ++i;
             cli.generate_tests = true;
             if (i < argc) {
@@ -48,6 +53,7 @@ cli_parameters_t resolve_cli_parameters(int argc, char** argv) {
                 PARAM_ERROR(-2, "Wrong --source usage.\n", cli);
             }
             cli.source = argv[i];
+            overrided_src = true;
         }
         else if (strcmp(argv[i], "--dest") == 0) {
             ++i;
@@ -55,16 +61,30 @@ cli_parameters_t resolve_cli_parameters(int argc, char** argv) {
                 PARAM_ERROR(-3, "Wrong --destination usage.\n", cli);
             }
             cli.destination = argv[i];
+            overrided_dest = true;
         }
         else if (strcmp(argv[i], "-e") == 0 || strcmp(argv[i], "--encode") == 0) {
+            if(cli.action != NONE) {
+                PARAM_ERROR(-5, "Can`t set action twice or overriding it.\n", cli);
+            }
             cli.action = ENCODE;
-            cli.source = DEFAULT_ORIGINAL;
-            cli.destination = DEFAULT_ENCODED;
+            if(!overrided_src)
+                cli.source = DEFAULT_ORIGINAL;
+            if(!overrided_dest)
+                cli.destination = DEFAULT_ENCODED;
         }
         else if (strcmp(argv[i], "-d") == 0 || strcmp(argv[i], "--decode") == 0) {
+            if(cli.action != NONE) {
+                PARAM_ERROR(-5, "Can`t set action twice or overriding it.\n", cli);
+            }
+            if(cli.generate_tests) {
+                PARAM_ERROR(-4, "Wrong CLI parameters: can`t generate tests if decoding.\n", cli);
+            }
             cli.action = DECODE;
-            cli.destination = DEFAULT_ORIGINAL;
-            cli.source = DEFAULT_ENCODED;
+            if(!overrided_src)
+                cli.destination = DEFAULT_ORIGINAL;
+            if(!overrided_dest)
+                cli.source = DEFAULT_ENCODED;
         }
         else {
             PARAM_ERROR(-100, "Undefined CLI parameter.\n", cli);
