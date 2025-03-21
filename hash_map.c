25d9eb1f00096c1b76ba6afebc32d5eff106b838
leedoor <samoshenkoleonid@yandex.ru>
2025-03-21 11:16:05 +0300

    essential_hash_functions

--- hash_map.c
+++ hash_map.c
@@ -1,25 +1,38 @@
 #include <stddef.h>
 #include <stdlib.h>
+#include <wchar.h>
 
-typedef char pair_t[2];
-typedef pair_t* bucket_t; 
-typedef bucket_t* buckets_t;
+typedef int hvalue_t;
+typedef wchar_t hkey_t[2];
+typedef struct {
+    hkey_t key;
+    hvalue_t val;
+} hkey_value_t;
+typedef hkey_value_t* bucket_t; 
 typedef struct {
     size_t size;
     size_t  capacity;
-    buckets_t buckets;
+    bucket_t* buckets;
 } hash_map_t;
+#define DEFAULT_CAPACITY 16
+#define MAX_BUCKET_OVERFLOW 5
+size_t hash_wchar_pair(wchar_t pair[2]) {
+    return 2;
+}
 
 hash_map_t* hash_map_new() {
     return (hash_map_t*) malloc(sizeof(hash_map_t));
 }
+void buckets_ctor(bucket_t** buckets, size_t size) {
+    *buckets = (bucket_t*)malloc(sizeof(bucket_t) * size);
+    for(size_t i = 0; i < size; ++i) {
+        (*buckets)[i] = (hkey_value_t*) malloc(sizeof(hkey_value_t) * MAX_BUCKET_OVERFLOW);
+    }
+}
 void hash_map_ctor(hash_map_t* hm) {
     hm->size = 0;
-    hm->capacity = 16;
-    hm->buckets = (bucket_t*)malloc(sizeof(bucket_t) * hm->capacity);
-    for(size_t i = 0; i < hm->capacity; ++i) {
-        hm->buckets[i] = (pair_t*) malloc(sizeof(char[2]) * 5);
-    }
+    hm->capacity = DEFAULT_CAPACITY;
+    buckets_ctor(&hm->buckets, DEFAULT_CAPACITY);
 }
 void hash_map_dtor(hash_map_t* hm) {
     for(int i = 0; i < hm->capacity; ++i) {
@@ -28,6 +41,42 @@ void hash_map_dtor(hash_map_t* hm) {
     free(hm->buckets);
 }
 
-void hash_map_insert(hash_map_t* hm, int k, int v);
-void hash_map_remove(hash_map_t* hm, int k);
-int* hash_map_get(hash_map_t* hm, int k);
+int hash_map_reserve(hash_map_t* hm, size_t new_capacity){
+    if(new_capacity == hm->capacity) return 0;
+    size_t new_size = hm->size < new_capacity ? hm->size : new_capacity;
+    bucket_t* bucketscp;
+    buckets_ctor(&bucketscp, new_capacity); 
+    if(bucketscp == NULL) {
+        return -1;
+    }
+    for(size_t i = 0; i < new_size; ++i) {
+        bucketscp[i] = hm->buckets[i];
+    }
+    free(hm->buckets);
+    hm->buckets = bucketscp;
+    hm->capacity = new_capacity;
+    hm->size = new_size;
+    return 0;
+}
+int hash_map_resize(hash_map_t* hm, size_t new_size) {
+    if(new_size == hm->size) return 0;
+    if(new_size < hm->capacity) {
+        hm->size = new_size;
+        return 0;
+    }
+        
+    return 0;
+}
+
+void hash_map_insert(hash_map_t* hm, hkey_t k, hvalue_t v) {
+    size_t hash_idx = hash_wchar_pair(k);
+    hkey_value_t hkv;
+    wcsncpy(hkv.key, k, 2);
+    hkv.val = v;
+
+    if(hm->capacity <= hm->size) {
+        
+    }
+}
+void hash_map_remove(hash_map_t* hm, hkey_t  k);
+int* hash_map_get(hash_map_t* hm, hkey_t k);
